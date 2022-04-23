5e78e60892b730456b0959f64405a7e05804515b
Stacyde <degtich@mail.ru>
2022-04-23 16:54:27 +0400

    added JUnit4

--- src/test/java/ru/netology/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/CashbackHackServiceTest.java
@@ -1,8 +1,8 @@
 package ru.netology;
 
-import org.testng.annotations.Test;
+import org.junit.Test;
 
-import static org.testng.Assert.*;
+import static org.junit.Assert.*;
 
 public class CashbackHackServiceTest {
 
@@ -12,7 +12,17 @@ public class CashbackHackServiceTest {
         int amount = 900;
         int actual = cashbackHackService.remain(amount);
         int expected = 100;
-        assertEquals(actual, expected);
+        assertEquals(expected, actual);
+
+    }
+    @Test
+    public void Service1000() {
+        CashbackHackService cashbackHackService = new CashbackHackService();
+        int amount = 1000;
+        int actual = cashbackHackService.remain(amount);
+        int expected = 0;
+        assertEquals(expected, actual);
+
     }
 
 }
